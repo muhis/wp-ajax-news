ca46bef0bb0c095c1088c6c5f3b31ad95985de2d
Mohi <m.salman@zinkki.com>
2016-12-29 19:27:19 +0200

    changed the plugin structure, removed test.html due to the lack of need. Remove AJAX codes from main.js because it is not needed now. Rename the bxslider folder into /js. Bulding a new news frame.

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
-var btn = document.getElementById('btn');
-var demo = document.getElementById('demo')
-btn.addEventListener('click',function(){
-	var ourRequest = new XMLHttpRequest();
-	  ourRequest.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-	     var ourData = JSON.parse(ourRequest.responseText);
-	     console.log(ourData);
-	    }
-	  };
-	  ourRequest.open("GET", "https://learnwebcode.github.io/json-example/animals-1.json", true);
-	  ourRequest.send();
-})
-
-function renderHTML(data){
-	for (i=0; i< data.length; i++){
-		demo.insertAdjacentHTML('beforeend','</br>' + data[i].name);
-	}
-	
-}
+$(document).ready(function(){
+  $('.slider5').bxSlider({
+    slideWidth: 900,
+    minSlides: 4,
+    maxSlides: 6,
+    moveSlides: 3,
+    slideMargin: 10,
+		responsive: false
+  });
+});
